6dfe57032d78c1510c722c1d208080f09bc48e8c
Chet Corcos <ccorcos@gmail.com>
2020-03-19 11:00:19 -0700

    First

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.PureComponent {
+	componentWillMount() {
+		fetch(
+			"https://gist.githubusercontent.com/simonlast/d5a86ba0c82e1b0d9f6e3d2581b95755/raw/f608b9b896dd3339df13dae317998d5f24c00a50/edu-scorecard.csv"
+		).then(contents => {
+			console.log(contents)
+		})
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	render() {
+		return <div>Hello World!</div>
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
